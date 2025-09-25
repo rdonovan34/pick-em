93faa193350d97b5b87d08bd301dbafbeea18c95
Ryan Donovan <rad9vy@virginia.edu>
2025-09-25 13:40:58 -0400

    new Xcode Project

---
+++ PickEm/PickEm/PickEmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickEmApp.swift
+//  PickEm
+//
+//  Created by Ryan Donovan on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PickEmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PickEm/PickEmTests/PickEmTests.swift
@@ -0,0 +1,17 @@
+//
+//  PickEmTests.swift
+//  PickEmTests
+//
+//  Created by Ryan Donovan on 9/25/25.
+//
+
+import Testing
+@testable import PickEm
+
+struct PickEmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
